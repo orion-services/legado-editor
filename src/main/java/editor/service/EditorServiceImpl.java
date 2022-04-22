8315c49d8aefd19a2b5b2c3afa7b2decce910ea0
guilherme.moreira <guilherme.moreira@dbserver.com.br>
2022-04-22 13:53:49 -0300

    readme

--- src/main/java/editor/service/EditorServiceImpl.java
+++ src/main/java/editor/service/EditorServiceImpl.java
@@ -21,7 +21,6 @@ package editor.service;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
